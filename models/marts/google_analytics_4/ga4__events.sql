e94a92b68ea83ccdce69cc6957f435834a416f6a
ryan@systematik.ca <ryan@systematik.ca>
2023-05-30 15:13:28 +0000

    updated sessions in ga4__events

--- models/marts/google_analytics_4/ga4__events.sql
+++ models/marts/google_analytics_4/ga4__events.sql
@@ -16,8 +16,28 @@ with events as (
 )
 
 SELECT 
-  events.*,
-  sessions.* except (session_key)
+    events.*,
+    sessions.session_start_date,
+    sessions.session_start_timestamp,
+    sessions.landing_page_path,
+    sessions.landing_page,
+    sessions.landing_page_hostname,
+    sessions.landing_page_referrer,
+    sessions.is_first_session,
+    sessions.session_source,
+    sessions.session_medium,
+    sessions.session_campaign,
+    sessions.session_content,
+    sessions.session_term,
+    sessions.session_default_channel_grouping,
+    sessions.session_source_category,
+    sessions.sys_session_ad_group_id,
+    sessions.sys_session_ad_id,
+    sessions.sys_session_placement_id,
+    sessions.sys_session_landing_page,
+    sessions.sys_session_landing_page_path,
+    sessions.sys_session_exit_page,
+    sessions.sys_session_exit_page_path
 FROM events
 left join  sessions 
   on events.session_key = sessions.session_key
