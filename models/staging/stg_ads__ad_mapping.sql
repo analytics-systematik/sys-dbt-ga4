bce2754504fdf583f5fcc9d2fb032de5ba1ffff7
ryan@systematik.ca <ryan@systematik.ca>
2023-10-04 18:44:57 +0000

    added ad mapping

---
+++ models/staging/stg_ads__ad_mapping.sql
@@ -0,0 +1,15 @@
+{% if var('enable_fivetran_ad_report_mapping', True) %}
+    {{ config(enabled=true, schema='stg_google_analytics_4') }}
+{% else %}
+    {{ config(enabled=false) }}
+{% endif %}
+
+with ad_reporting__ad_report as (
+    select 
+        distinct
+        ad_id,
+        ad
+    from {{ ref('ad_reporting__ad_report') }}
+)
+
+select * from ad_reporting__ad_report
---
+++ models/staging/stg_ads__campaign_mapping.sql
@@ -0,0 +1,15 @@
+{% if var('enable_fivetran_ad_report_mapping', True) %}
+    {{ config(enabled=true, schema='stg_google_analytics_4') }}
+{% else %}
+    {{ config(enabled=false) }}
+{% endif %}
+
+with ad_reporting__ad_report as (
+    select 
+        distinct
+        campaign_id,
+        campaign
+    from {{ ref('ad_reporting__ad_report') }}
+)
+
+select * from ad_reporting__ad_report
