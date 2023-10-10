e4f4fecb6f899af5c6e8f1100658753816213db9
ryan@systematik.ca <ryan@systematik.ca>
2023-10-10 19:16:41 +0000

    added paid_ads__ad_report_enriched

---
+++ models/marts/marketing/paid_ads__ad_report_enriched.sql
@@ -0,0 +1,48 @@
+{% if var('enable_fivetran_ad_report_mapping', True) %}
+    {{ config(enabled=true, schema='marts_marketing') }}
+{% else %}
+    {{ config(enabled=false) }}
+{% endif %}
+
+with paid_ads as (
+    select
+        date_day,
+        platform,
+        account_name as account,
+        campaign_name as campaign,
+        ad_set_name as ad_group,
+        ad_name as ad,
+        cast(null as {{ dbt.type_string() }}) as session_source,
+        cast(null as {{ dbt.type_string() }}) as session_medium,
+        clicks,
+        impressions,
+        spend,
+        null as conversions,
+        null as revenue
+    from {{ref('ad_reporting__ad_report')}} 
+),
+ga4_sessions_with_purchases as (
+    select 
+        session_start_date as date_day,
+        null as platform,
+        cast(null as {{ dbt.type_string() }}) as account,
+        session_campaign as campaign,
+        session_ad_group as ad_group,        	
+        session_ad_name as ad,
+        session_source,
+        session_medium,
+        null as clicks,
+        null as impressions,
+        null as spend,
+        conversions,
+        revenue
+    from {{ ref('ga4__sessions') }}
+    where count_purchase > 0
+),
+final as (
+    select * from paid_ads
+    union all
+    select * from ga4_sessions_with_purchases
+)
+
+select * from final
