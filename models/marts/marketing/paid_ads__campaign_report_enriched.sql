8dadabb13c6fdf98939170b63581d601f506dd91
ryan <116585343+ryan-systematik@users.noreply.github.com>
2025-11-12 00:05:56 +0800

    update paid_ads__campaing_report_enriched

--- models/marts/marketing/paid_ads__campaign_report_enriched.sql
+++ models/marts/marketing/paid_ads__campaign_report_enriched.sql
@@ -9,6 +9,7 @@
 with paid_ads as (
     select
         date_day,
+        campaign_id,
         platform,
         account_name as account,
         campaign_name as campaign,
@@ -18,29 +19,45 @@ with paid_ads as (
         impressions,
         spend,
         null as conversions,
-        null as revenue
+        null as revenue,
+        null as sessions
     from {{ref('ad_reporting__campaign_report')}} 
 ),
-ga4_sessions_with_purchases as (
+ga4_sessions_aggregated as (
     select 
         session_start_date as date_day,
-        cast(null as {{ dbt.type_string() }}) as platform,
-        cast(null as {{ dbt.type_string() }}) as account,
-        session_campaign as campaign,
-        session_source,
-        session_medium,
-        null as clicks,
-        null as impressions,
-        null as spend,
-        {{ conversions_field }} as conversions,
-        sum_event_value_in_usd as revenue
+        session_utm_id as campaign_id,
+        max(case
+            when session_source = "facebook" then "meta"
+            else session_source
+        end) as session_source,
+        max(session_medium) as session_medium,
+        sum(case when {{ conversions_field }} > 0 then {{ conversions_field }} else 0 end) as conversions,
+        sum(case when {{ conversions_field }} > 0 then sum_event_value_in_usd else 0 end) as revenue,
+        sum(sessions) as sessions
     from {{ ref('ga4__sessions') }}
-    where {{ conversions_field }} > 0
+    where session_utm_id is not null
+    group by 1, 2
 ),
 final as (
-    select * from paid_ads
-    union all
-    select * from ga4_sessions_with_purchases
+    select
+        coalesce(paid_ads.date_day, ga4_sessions_aggregated.date_day) as date_day,
+        coalesce(paid_ads.campaign_id, ga4_sessions_aggregated.campaign_id) as campaign_id,
+        paid_ads.platform,
+        paid_ads.account,
+        paid_ads.campaign,
+        ga4_sessions_aggregated.session_source,
+        ga4_sessions_aggregated.session_medium,
+        paid_ads.clicks,
+        paid_ads.impressions,
+        paid_ads.spend,
+        ga4_sessions_aggregated.conversions,
+        ga4_sessions_aggregated.revenue,
+        ga4_sessions_aggregated.sessions
+    from paid_ads
+    full outer join ga4_sessions_aggregated
+        on paid_ads.campaign_id = ga4_sessions_aggregated.campaign_id
+        and paid_ads.date_day = ga4_sessions_aggregated.date_day
 )
 
 select * from final
